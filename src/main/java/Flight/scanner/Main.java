679f5857c89e689bda986d05cd37d6a85efd9c68
RobertoJavaDev <robertojavadev@gmail.com>
2020-12-06 00:30:09 +0100

    added stage "flight scanner" to main class

--- src/main/java/Flight/scanner/Main.java
+++ src/main/java/Flight/scanner/Main.java
@@ -6,12 +6,16 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
+    private static final String BORDER_PANE_MAIN_FXML = "/fxml/BorderPaneMain.fxml";
 
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Flight scanner");
+        primaryStage.show();
     }
 
-
-    public void start(Stage stage) throws Exception {
+    public static void main(String[] args) {
+        launch(args);
 
     }
 }
