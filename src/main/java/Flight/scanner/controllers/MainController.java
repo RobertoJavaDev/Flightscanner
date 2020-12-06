0f4e2cb76e6f9d95a8227be498920796dca59888
RobertoJavaDev <robertojavadev@gmail.com>
2020-12-06 15:50:06 +0100

    added initialize method to MainController class

--- src/main/java/Flight/scanner/controllers/MainController.java
+++ src/main/java/Flight/scanner/controllers/MainController.java
@@ -1,10 +1,22 @@
 package Flight.scanner.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
+import javax.swing.border.Border;
 
 public class MainController {
 
     @FXML
+    private BorderPane borderPane;
+
+    @FXML
     private TopMenuButtonsController topMenuButtonsController;
+
+    @FXML
+    private void initialize(){
+        topMenuButtonsController.setMainController(this);
+
+    }
 
 }
--- src/main/java/Flight/scanner/controllers/TopMenuButtonsController.java
+++ src/main/java/Flight/scanner/controllers/TopMenuButtonsController.java
@@ -3,18 +3,29 @@ package Flight.scanner.controllers;
 import javafx.event.ActionEvent;
 
 public class TopMenuButtonsController {
+
+    private MainController mainController;
+
     public void openListOfFlights(ActionEvent actionEvent) {
+
     }
 
     public void openListOfPrices(ActionEvent actionEvent) {
+
     }
 
     public void addCity(ActionEvent actionEvent) {
+
     }
 
     public void addAirline(ActionEvent actionEvent) {
+
     }
 
     public void openListOfCities(ActionEvent actionEvent) {
+    }
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
     }
 }
