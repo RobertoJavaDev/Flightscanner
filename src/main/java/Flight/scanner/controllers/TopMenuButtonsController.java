2f42dc36c9b716ffd9efcc8f5f8677dd4f82ff6e
RobertoJavaDev <robertojavadev@gmail.com>
2020-12-06 13:04:38 +0100

    added new file TopMenuButtonsController with method

---
+++ src/main/java/Flight/scanner/controllers/TopMenuButtonsController.java
@@ -0,0 +1,20 @@
+package Flight.scanner.controllers;
+
+import javafx.event.ActionEvent;
+
+public class TopMenuButtonsController {
+    public void openListOfFlights(ActionEvent actionEvent) {
+    }
+
+    public void openListOfPrices(ActionEvent actionEvent) {
+    }
+
+    public void addCity(ActionEvent actionEvent) {
+    }
+
+    public void addAirline(ActionEvent actionEvent) {
+    }
+
+    public void openListOfCities(ActionEvent actionEvent) {
+    }
+}
