79bfdf5bab320874f522a3dda1be8793d24351bb
RobertoJavaDev <robertojavadev@gmail.com>
2020-12-12 15:58:31 +0100

    Added resetToggleButtons method to TopMenuButtonsController

--- src/main/java/Flight/scanner/controllers/TopMenuButtonsController.java
+++ src/main/java/Flight/scanner/controllers/TopMenuButtonsController.java
@@ -1,31 +1,51 @@
 package Flight.scanner.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
 
 public class TopMenuButtonsController {
 
     private MainController mainController;
 
+    @FXML
+    private ToggleGroup toggleButtons;
+
+    @FXML
     public void openListOfFlights(ActionEvent actionEvent) {
-
     }
 
+    @FXML
     public void openListOfPrices(ActionEvent actionEvent) {
-
     }
 
-    public void addCity(ActionEvent actionEvent) {
-
-    }
-
-    public void addAirline(ActionEvent actionEvent) {
-
-    }
-
+    @FXML
     public void openListOfCities(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void addCity(ActionEvent actionEvent) {
+        resetToggleButtons();
+    }
+
+    @FXML
+    public void addAirline(ActionEvent actionEvent) {
+        resetToggleButtons();
+    }
+
+    public void addConfig(ActionEvent actionEvent) {
+        resetToggleButtons();
+    }
+
+    private void resetToggleButtons(){
+        if (toggleButtons.getSelectedToggle() != null) {
+            toggleButtons.getSelectedToggle().setSelected(false);
+        }
     }
 
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
     }
+
+
 }
