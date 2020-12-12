e866bd3b45b3332f0f817777c87477ae0ca26098
RobertoJavaDev <robertojavadev@gmail.com>
2020-12-12 22:01:26 +0100

    refactored class MainController

--- src/main/java/Flight/scanner/controllers/MainController.java
+++ src/main/java/Flight/scanner/controllers/MainController.java
@@ -1,19 +1,16 @@
 package Flight.scanner.controllers;
 
 import Flight.scanner.utils.DialogsUtils;
+import Flight.scanner.utils.FxmlUtils;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckMenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
-import javax.swing.border.Border;
 import java.io.IOException;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -34,16 +31,7 @@ public class MainController {
     }
 
     public void setCenter (String fxmlPath){
-        FXMLLoader loader = new FXMLLoader (this.getClass().getResource(fxmlPath));
-        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages_eng");
-        loader.setResources(bundle);
-        Parent parent = null;
-        try {
-            parent = loader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        borderPane.setCenter(parent);
+        borderPane.setCenter(FxmlUtils.fxmlLoader(fxmlPath));
     }
 
 
