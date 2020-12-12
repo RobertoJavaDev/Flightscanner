b65e6093acda3cf82f699131a6ed97cc3fe7d70c
RobertoJavaDev <robertojavadev@gmail.com>
2020-12-12 21:51:49 +0100

    refactored main class

--- src/main/java/Flight/scanner/Main.java
+++ src/main/java/Flight/scanner/Main.java
@@ -1,27 +1,21 @@
 package Flight.scanner;
 
+import Flight.scanner.utils.FxmlUtils;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-import java.util.Locale;
-import java.util.ResourceBundle;
 
 public class Main extends Application {
 
     private static final String BORDER_PANE_MAIN_FXML = "/fxml/BorderPaneMain.fxml";
 
     public void start(Stage primaryStage) throws Exception {
+        Pane borderPane = FxmlUtils.fxmlLoader(BORDER_PANE_MAIN_FXML);
 
-        FXMLLoader loader = new FXMLLoader (this.getClass().getResource(BORDER_PANE_MAIN_FXML));
-        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages_eng");
-        loader.setResources(bundle);
-        BorderPane borderPane = loader.load();
         Scene scene = new Scene(borderPane);
         primaryStage.setScene(scene);
-        primaryStage.setTitle(bundle.getString("title.application"));
+        primaryStage.setTitle(FxmlUtils.getResourceBundle().getString("title.application"));
         primaryStage.show();
     }
 
