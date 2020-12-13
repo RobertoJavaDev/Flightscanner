9f35b5c9bd787d47d23d7f7a894941610c51a9ef
RobertoJavaDev <robertojavadev@gmail.com>
2020-12-13 18:27:18 +0100

    added method saveAirlineInDatabase

--- src/main/java/Flight/scanner/modelFX/AirlineModel.java
+++ src/main/java/Flight/scanner/modelFX/AirlineModel.java
@@ -1,4 +1,17 @@
 package Flight.scanner.modelFX;
 
+import Flight.scanner.database.dao.AirlineDao;
+import Flight.scanner.database.dbutils.DbManager;
+import Flight.scanner.database.models.Airline;
+import Flight.scanner.database.models.BaseModel;
+
 public class AirlineModel {
+
+    public void saveAirlineInDataBase(String name){
+        AirlineDao airlineDao = new AirlineDao(DbManager.getConnectionSource());
+        Airline airline = new Airline();
+        airline.setAirline(name);
+        airlineDao.creatOrUpdate((BaseModel) airline);
+        DbManager.closeConnectionSource();
+    }
 }
