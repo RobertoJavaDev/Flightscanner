d5f305545259a61515e6bdfd4f006eadcdc4634b
RobertoJavaDev <robertojavadev@gmail.com>
2020-12-13 18:10:07 +0100

    added two properties id & airline to class AirlineFx

--- src/main/java/Flight/scanner/modelFX/AirlineFx.java
+++ src/main/java/Flight/scanner/modelFX/AirlineFx.java
@@ -1,4 +1,36 @@
 package Flight.scanner.modelFX;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class AirlineFx {
+
+    private IntegerProperty id = new SimpleIntegerProperty();
+    private StringProperty airline = new SimpleStringProperty();
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getAirline() {
+        return airline.get();
+    }
+
+    public StringProperty airlineProperty() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline.set(airline);
+    }
 }
