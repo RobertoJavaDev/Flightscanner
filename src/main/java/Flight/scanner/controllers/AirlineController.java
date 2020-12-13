f05b689b138348296e7c7accb6a93a46f822dd53
RobertoJavaDev <robertojavadev@gmail.com>
2020-12-13 17:22:59 +0100

    implemented of elements form Airline.fxml

--- src/main/java/Flight/scanner/controllers/AirlineController.java
+++ src/main/java/Flight/scanner/controllers/AirlineController.java
@@ -1,8 +1,22 @@
 package Flight.scanner.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 
 public class AirlineController {
+
+    @FXML
+    private Button addAirlineButton;
+
+    @FXML
+    private TextField airlineTextField;
+
+    @FXML
+    private ComboBox<String> airlineCombobox;
 
 
     public void addAirlineOnActon(ActionEvent actionEvent) {
