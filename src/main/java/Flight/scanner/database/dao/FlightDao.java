7acb3c168bc722653891bdaca4ec72eb2493da0a
RobertoJavaDev <robertojavadev@gmail.com>
2020-12-13 11:56:32 +0100

    added method flightDao

--- src/main/java/Flight/scanner/database/dao/FlightDao.java
+++ src/main/java/Flight/scanner/database/dao/FlightDao.java
@@ -1,4 +1,9 @@
 package Flight.scanner.database.dao;
 
-public class FlightDao {
+import com.j256.ormlite.support.ConnectionSource;
+
+public class FlightDao extends CommonDao {
+    public FlightDao(ConnectionSource connectionSource) {
+        super(connectionSource);
+    }
 }
