44d64d6c2c232408961125494d27a7aa57ea5437
RobertoJavaDev <robertojavadev@gmail.com>
2020-12-14 21:13:02 +0100

    added method init - init to comboBox

--- src/main/java/Flight/scanner/modelFX/AirlineModel.java
+++ src/main/java/Flight/scanner/modelFX/AirlineModel.java
@@ -4,8 +4,30 @@ import Flight.scanner.database.dao.AirlineDao;
 import Flight.scanner.database.dbutils.DbManager;
 import Flight.scanner.database.models.Airline;
 import Flight.scanner.database.models.BaseModel;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.List;
 
 public class AirlineModel {
+
+    private ObservableList<AirlineFx> airlineList = FXCollections.observableArrayList();
+    private ObjectProperty<AirlineFx> airline = new SimpleObjectProperty<>();
+
+    public void init(){
+        AirlineDao airlineDao = new AirlineDao(DbManager.getConnectionSource());
+        List<Airline> airlines = airlineDao.queryForAll(Airline.class);
+        airlines.forEach(c->{
+            AirlineFx airlineFx = new AirlineFx();
+            airlineFx.setId(c.getId());
+            airlineFx.setAirline(c.getAirline());
+            this.airlineList.add(airlineFx);
+
+        });
+        DbManager.closeConnectionSource();
+    }
 
     public void saveAirlineInDataBase(String name){
         AirlineDao airlineDao = new AirlineDao(DbManager.getConnectionSource());
