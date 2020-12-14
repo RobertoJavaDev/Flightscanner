fefc6f29d94108cf6bd7ed90f41f0170c4196c94
RobertoJavaDev <robertojavadev@gmail.com>
2020-12-14 21:50:10 +0100

    added method disable delete button

--- src/main/java/Flight/scanner/controllers/AirlineController.java
+++ src/main/java/Flight/scanner/controllers/AirlineController.java
@@ -33,8 +33,8 @@ public class AirlineController {
     }
 
     private void initBindings() {
-        addAirlineButton.disableProperty().bind(airlineTextField.textProperty().isEmpty());
-
+        this.addAirlineButton.disableProperty().bind(airlineTextField.textProperty().isEmpty());
+        this.deleteAirlineButton.disableProperty().bind(this.airlineModel.airlineProperty().isNull());
     }
 
     public void addAirlineOnActon(ActionEvent actionEvent) {
